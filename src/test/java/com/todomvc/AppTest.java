4d63f9335b7e2ab6b47a70639eade40265c1dae2
2019DEV091 <2019DEV091@gmail.com>
2021-08-03 21:04:23 +0200

    selenium trial

--- src/test/java/com/todomvc/AppTest.java
+++ src/test/java/com/todomvc/AppTest.java
@@ -3,6 +3,9 @@ package com.todomvc;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Unit test for simple App.
  */
@@ -12,6 +15,21 @@ public class AppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
+
         assertTrue(true);
+
+        WebDriver driver = new ChromeDriver();
+
+        
+            driver.get("https://todomvc.com/examples/angularjs/#/");
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        driver.quit();
+
     }
 }
