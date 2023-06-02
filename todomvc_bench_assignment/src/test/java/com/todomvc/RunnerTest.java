25e01523572ca590187caedc41dd69bfcfae9a2a
mryckmans <mryckmans@PF3MLXWT.ctg.com>
2023-06-02 18:24:45 +0200

    cleanup

--- todomvc_bench_assignment/src/test/java/com/todomvc/RunnerTest.java
+++ todomvc_bench_assignment/src/test/java/com/todomvc/RunnerTest.java
@@ -1,14 +1,12 @@
 package com.todomvc;
 
-import org.junit.jupiter.api.TestInstance;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
-
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 
 
@@ -17,7 +15,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("com/todomvc")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key=GLUE_PROPERTY_NAME, value="com/todomvc")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RunnerTest{
 
     
